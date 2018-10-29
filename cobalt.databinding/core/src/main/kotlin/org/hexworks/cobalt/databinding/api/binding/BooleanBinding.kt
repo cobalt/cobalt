128c159bf09e13974f93810b3b5193fcd5f339d1
adam-arold <arold.adam@gmail.com>
2018-10-30 00:21:28 +0100

    progress

--- cobalt.databinding/core/src/main/kotlin/org/hexworks/cobalt/databinding/api/binding/BooleanBinding.kt
+++ cobalt.databinding/core/src/main/kotlin/org/hexworks/cobalt/databinding/api/binding/BooleanBinding.kt
@@ -1,12 +1,11 @@
 package org.hexworks.cobalt.databinding.api.binding
 
 import org.hexworks.cobalt.databinding.api.event.ChangeListener
-import org.hexworks.cobalt.databinding.api.expression.BooleanExpression
 import org.hexworks.cobalt.events.Subscription
 
-abstract class BooleanBinding : BooleanExpression(), Binding<Boolean> {
+abstract class BooleanBinding : Binding<Boolean> {
 
-    override fun onChange(listener: ChangeListener<in Boolean>): Subscription {
+    override fun onChange(listener: ChangeListener<Boolean>): Subscription {
         TODO()
     }
 
